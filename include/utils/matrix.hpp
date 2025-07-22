961f7fb803efff0a30e2ebdb1dbb5929a2548267
Icbitic <icbitic@foxmail.com>
2025-07-23 02:20:52 +0800

    install xtensor in actions

--- include/utils/matrix.hpp
+++ include/utils/matrix.hpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <vector>
 #include <xtensor-blas/xlinalg.hpp>
-#include <xtensor/xarray.hpp>
 #include <xtensor/xio.hpp>
 #include <xtensor/xmath.hpp>
 #include <xtensor/xrandom.hpp>
